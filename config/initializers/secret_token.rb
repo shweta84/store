aea12e408af0059aad9e4f165c18def22859d892
Shweta <ruby.rails3@gmail.com>
2013-05-07 13:49:35 +0530

    all committed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '9731228238fb387c72199647e7a3dfdffa90e9a85b6553f00f3ae52ff2acfa53da160d327c0cb6563d1b29ad62d7a290d23950857fa6f4101b2d70e34a743125'
